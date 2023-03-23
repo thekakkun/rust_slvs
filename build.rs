08534ee9a8e770af6e3cdcb1ae63533f176c440f
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-03-23 14:27:58 -0400

    Don't specify static

--- build.rs
+++ build.rs
@@ -13,45 +13,27 @@ fn main() {
     let headers_path = libdir_path.join("include/slvs.h");
     let headers_path_str = headers_path.to_str().expect("Path is not a valid string.");
     let lib_path = libdir_path.join("build").join("bin");
+    // println!("cargo:rustc-link-search={}", lib_path.to_str().unwrap());
+    // println!("cargo:rustc-link-lib=static=slvs");
+    // println!("cargo:rustc-link-lib=static=mimalloc");
+
+    // let stdlib_path = PathBuf::from("/usr/lib/gcc/x86_64-linux-gnu/11");
+    // println!("cargo:rustc-link-search={}", stdlib_path.to_str().unwrap());
+    // println!("cargo:rustc-link-lib=static=gomp");
+    // println!("cargo:rustc-link-lib=static=stdc++");
 
     println!("cargo:rustc-link-search={}", lib_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=slvs");
     println!("cargo:rustc-link-lib=mimalloc");
-    println!("cargo:rustc-link-lib=stdc++");
+    
+    // println!("cargo:rustc-link-search={}", stdlib_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=gomp");
+    println!("cargo:rustc-link-lib=stdc++");
     println!("cargo:rerun-if-changed={}", headers_path_str);
 
-    // let obj_path = libdir_path.join("build/slvs.o");
-    // let lib_path = libdir_path.join("build/libslvs.a");
-
-    // if !std::process::Command::new("clang++")
-    //     .arg("-c")
-    //     .arg("-o")
-    //     .arg(&obj_path)
-    //     .arg(libdir_path.join("src/lib.cpp"))
-    //     .output()
-    //     .expect("Could not spawn `clang`.")
-    //     .status
-    //     .success()
-    // {
-    //     panic!("Could not compile object file.");
-    // }
-
-    // if !std::process::Command::new("ar")
-    //     .arg("rcs")
-    //     .arg(lib_path)
-    //     .arg(obj_path)
-    //     .output()
-    //     .expect("Could not spawn `ar`.")
-    //     .status
-    //     .success()
-    // {
-    //     panic!("Could not emit library file.");
-    // }
-
     let bindings = bindgen::Builder::default()
-        .header(headers_path_str)
         .opaque_type("std::.*")
+        .header(headers_path_str)
         .clang_arg("-x")
         .clang_arg("c++")
         .clang_arg("-std=c++11")
