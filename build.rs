929bbc987f78a1558f4e60b47fee9680305fc266
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-03-27 13:56:14 -0400

    Build slvs library using cc

--- build.rs
+++ build.rs
@@ -10,21 +10,57 @@ fn main() {
         .canonicalize()
         .expect("Cannot canonicalize path.");
 
-    let headers_path = libdir_path.join("include/slvs.h");
-    let headers_path_str = headers_path.to_str().expect("Path is not a valid string.");
-    let lib_path = libdir_path.join("build/bin");
+    cc::Build::new()
+        .cpp(true)
+        .flag("-DLIBRARY")
+        .include(libdir_path.join("src"))
+        .include(libdir_path.join("include"))
+        .include(libdir_path.join("build/src"))
+        .include(libdir_path.join("extlib/eigen"))
+        .include(libdir_path.join("extlib/mimalloc/include"))
+        .flag("-Wno-unused-parameter")
+        .flag("-Wno-missing-field-initializers")
+        .files(
+            [
+                "src/util.cpp",
+                "src/entity.cpp",
+                "src/expr.cpp",
+                "src/constraint.cpp",
+                "src/constrainteq.cpp",
+                "src/system.cpp",
+                "src/platform/platform.cpp",
+                "src/lib.cpp",
+            ]
+            .map(|file| libdir_path.join(file)),
+        )
+        .compile("slvs");
 
-    println!("cargo:rustc-link-search={}", lib_path.to_str().unwrap());
-    println!("cargo:rustc-link-lib=slvs");
-    println!("cargo:rustc-link-lib=mimalloc");
-
-    println!("cargo:rustc-link-lib=gomp");
-    println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rerun-if-changed={}", headers_path_str);
+    cc::Build::new()
+        .include(libdir_path.join("extlib/mimalloc/include"))
+        .files(
+            [
+                "extlib/mimalloc/src/stats.c",
+                "extlib/mimalloc/src/random.c",
+                "extlib/mimalloc/src/os.c",
+                "extlib/mimalloc/src/bitmap.c",
+                "extlib/mimalloc/src/arena.c",
+                "extlib/mimalloc/src/segment-cache.c",
+                "extlib/mimalloc/src/segment.c",
+                "extlib/mimalloc/src/page.c",
+                "extlib/mimalloc/src/alloc.c",
+                "extlib/mimalloc/src/alloc-aligned.c",
+                "extlib/mimalloc/src/alloc-posix.c",
+                "extlib/mimalloc/src/heap.c",
+                "extlib/mimalloc/src/options.c",
+                "extlib/mimalloc/src/init.c",
+            ]
+            .map(|file| libdir_path.join(file)),
+        )
+        .compile("mimalloc");
 
     let bindings = bindgen::Builder::default()
         .opaque_type("std::.*")
-        .header(headers_path_str)
+        .header(libdir_path.join("include/slvs.h").to_str().unwrap())
         .clang_arg("-x")
         .clang_arg("c++")
         .clang_arg("-std=c++11")
