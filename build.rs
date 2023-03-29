0f503f979b01cab599c3009d29036eec9efc2a03
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-03-29 09:59:09 -0400

    Builds on linux

--- build.rs
+++ build.rs
@@ -10,70 +10,60 @@ fn main() {
         .canonicalize()
         .expect("Cannot canonicalize path.");
 
+    // This is really just to create a config.h file.
+    // Would really like to be able to remove this, since it adds unnecessary steps/files to the package.
     let dst = Config::new("solvespace")
         .profile("Release")
         .build_target("slvs")
         .build();
 
-    println!(
-        "cargo:rustc-link-search=native={}",
-        dst.join("build/bin/Release").display()
-    );
-    println!(
-        "cargo:rustc-link-search={}",
-        PathBuf::from(r"C:\Windows\System32").to_str().unwrap()
-    );
-    println!("cargo:rustc-link-lib=slvs");
-    println!("cargo:rustc-link-lib=mimalloc-static");
-    println!("cargo:rustc-link-lib=shell32");
+    cc::Build::new()
+        .cpp(true)
+        .flag_if_supported("-DLIBRARY")
+        .include(libdir_path.join("src"))
+        .include(libdir_path.join("include"))
+        .include(dst.join("build/src"))
+        .include(libdir_path.join("extlib/eigen"))
+        .include(libdir_path.join("extlib/mimalloc/include"))
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-missing-field-initializers")
+        .files(
+            [
+                "src/util.cpp",
+                "src/entity.cpp",
+                "src/expr.cpp",
+                "src/constraint.cpp",
+                "src/constrainteq.cpp",
+                "src/system.cpp",
+                "src/platform/platform.cpp",
+                "src/lib.cpp",
+            ]
+            .map(|file| libdir_path.join(file)),
+        )
+        .compile("slvs");
 
-    // cc::Build::new()
-    //     .cpp(true)
-    //     .flag("-DLIBRARY")
-    //     .include(libdir_path.join("src"))
-    //     .include(libdir_path.join("include"))
-    //     .include(dst.join("build/src"))
-    //     .include(libdir_path.join("extlib/eigen"))
-    //     .include(libdir_path.join("extlib/mimalloc/include"))
-    //     .flag("-Wno-unused-parameter")
-    //     .flag("-Wno-missing-field-initializers")
-    //     .files(
-    //         [
-    //             "src/util.cpp",
-    //             "src/entity.cpp",
-    //             "src/expr.cpp",
-    //             "src/constraint.cpp",
-    //             "src/constrainteq.cpp",
-    //             "src/system.cpp",
-    //             "src/platform/platform.cpp",
-    //             "src/lib.cpp",
-    //         ]
-    //         .map(|file| libdir_path.join(file)),
-    //     )
-    //     .compile("slvs");
-
-    // cc::Build::new()
-    //     .include(libdir_path.join("extlib/mimalloc/include"))
-    //     .files(
-    //         [
-    //             "extlib/mimalloc/src/stats.c",
-    //             "extlib/mimalloc/src/random.c",
-    //             "extlib/mimalloc/src/os.c",
-    //             "extlib/mimalloc/src/bitmap.c",
-    //             "extlib/mimalloc/src/arena.c",
-    //             "extlib/mimalloc/src/segment-cache.c",
-    //             "extlib/mimalloc/src/segment.c",
-    //             "extlib/mimalloc/src/page.c",
-    //             "extlib/mimalloc/src/alloc.c",
-    //             "extlib/mimalloc/src/alloc-aligned.c",
-    //             "extlib/mimalloc/src/alloc-posix.c",
-    //             "extlib/mimalloc/src/heap.c",
-    //             "extlib/mimalloc/src/options.c",
-    //             "extlib/mimalloc/src/init.c",
-    //         ]
-    //         .map(|file| libdir_path.join(file)),
-    //     )
-    //     .compile("mimalloc");
+    cc::Build::new()
+        .include(libdir_path.join("extlib/mimalloc/include"))
+        .files(
+            [
+                "extlib/mimalloc/src/stats.c",
+                "extlib/mimalloc/src/random.c",
+                "extlib/mimalloc/src/os.c",
+                "extlib/mimalloc/src/bitmap.c",
+                "extlib/mimalloc/src/arena.c",
+                "extlib/mimalloc/src/segment-cache.c",
+                "extlib/mimalloc/src/segment.c",
+                "extlib/mimalloc/src/page.c",
+                "extlib/mimalloc/src/alloc.c",
+                "extlib/mimalloc/src/alloc-aligned.c",
+                "extlib/mimalloc/src/alloc-posix.c",
+                "extlib/mimalloc/src/heap.c",
+                "extlib/mimalloc/src/options.c",
+                "extlib/mimalloc/src/init.c",
+            ]
+            .map(|file| libdir_path.join(file)),
+        )
+        .compile("mimalloc");
 
     let bindings = bindgen::Builder::default()
         .opaque_type("std::.*")
