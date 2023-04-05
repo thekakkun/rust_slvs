0a974188afb13b7c9c0f17d3fcf32fba4d3e82a9
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-04-05 13:13:04 -0400

    Implement constructor for slvs_param

--- src/element/param.rs
+++ src/element/param.rs
@@ -1,39 +1,15 @@
-use std::rc::Weak;
+use std::sync::atomic::{AtomicU32, Ordering};
 
-use super::{group::Group, Handle};
+use crate::bindings;
 
-pub struct Param {
-    h: u32,
-    group: Weak<Group>,
-    val: f64,
-}
+static NEXT_PARAM_H: AtomicU32 = AtomicU32::new(1);
 
-impl Default for Param {
-    fn default() -> Self {
+impl bindings::Slvs_Param {
+    fn new(group: bindings::Slvs_hGroup, val: f64) -> Self {
         Self {
-            h: 0,
-            group: Weak::new(),
-            val: 0.0,
-        }
-    }
-}
-
-impl Param {
-    pub fn new(group: &Weak<Group>, val: f64) -> Self {
-        Self {
-            group: Weak::clone(group),
+            h: NEXT_PARAM_H.fetch_add(1, Ordering::SeqCst),
+            group,
             val,
-            ..Default::default()
         }
-    }
-}
-
-impl Handle for Param {
-    fn get_handle(&self) -> u32 {
-        self.h
-    }
-
-    fn set_handle(&mut self, h: u32) {
-        self.h = h;
     }
 }
