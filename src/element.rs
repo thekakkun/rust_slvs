aece4b11faf9ee5dcfe25678dcb44099efccb0f5
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-04-10 13:13:31 -0400

    Move group mod outside of element

--- src/element.rs
+++ src/element.rs
@@ -1,56 +1,56 @@
-use crate::binding;
+// use crate::binding;
 
-use self::{constraint::Constraint, entity::Entity, param::Param};
+// use self::{constraint::Constraint, entity::Entity, param::Param};
 
 pub mod constraint;
 pub mod entity;
-pub mod group;
+
 pub mod param;
 
-#[derive(Clone, Copy, Debug)]
-pub enum Handle {
-    Group(binding::Slvs_hGroup),
-    Param(binding::Slvs_hParam),
-    Entity(binding::Slvs_hEntity),
-    Constraint(binding::Slvs_hConstraint),
-}
+// #[derive(Clone, Copy, Debug)]
+// pub enum Handle {
+//     Group(binding::Slvs_hGroup),
+//     Param(binding::Slvs_hParam),
+//     Entity(binding::Slvs_hEntity),
+//     Constraint(binding::Slvs_hConstraint),
+// }
 
-impl From<Param> for Handle {
-    fn from(value: Param) -> Self {
-        Self::Param(value.h)
-    }
-}
+// impl From<Param> for Handle {
+//     fn from(value: Param) -> Self {
+//         Self::Param(value.h)
+//     }
+// }
 
-impl From<Entity> for Handle {
-    fn from(value: Entity) -> Self {
-        Self::Entity(value.h)
-    }
-}
+// impl From<Entity> for Handle {
+//     fn from(value: Entity) -> Self {
+//         Self::Entity(value.h)
+//     }
+// }
 
-impl From<Constraint> for Handle {
-    fn from(value: Constraint) -> Self {
-        Self::Constraint(value.h)
-    }
-}
+// impl From<Constraint> for Handle {
+//     fn from(value: Constraint) -> Self {
+//         Self::Constraint(value.h)
+//     }
+// }
 
-impl From<Handle> for u32 {
-    fn from(value: Handle) -> Self {
-        match value {
-            Handle::Group(h) | Handle::Param(h) | Handle::Entity(h) | Handle::Constraint(h) => h,
-        }
-    }
-}
+// impl From<Handle> for u32 {
+//     fn from(value: Handle) -> Self {
+//         match value {
+//             Handle::Group(h) | Handle::Param(h) | Handle::Entity(h) | Handle::Constraint(h) => h,
+//         }
+//     }
+// }
 
-pub(crate) trait PushReturn<T: Copy + Into<Handle>> {
-    fn push_return(&mut self, element: T) -> Handle;
-}
+// pub(crate) trait PushReturn<T: Copy + Into<Handle>> {
+//     fn push_return(&mut self, element: T) -> Handle;
+// }
 
-impl<T> PushReturn<T> for Vec<T>
-where
-    T: Copy + Into<Handle>,
-{
-    fn push_return(&mut self, element: T) -> Handle {
-        self.push(element);
-        element.into()
-    }
-}
+// impl<T> PushReturn<T> for Vec<T>
+// where
+//     T: Copy + Into<Handle>,
+// {
+//     fn push_return(&mut self, element: T) -> Handle {
+//         self.push(element);
+//         element.into()
+//     }
+// }
