1e7085d31d8bcde0be0819709a2a02283a6b9734
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-04-10 13:37:46 -0400

    Try implementation of updated system design

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,15 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-pub mod element;
+use constraint::Constraint;
+use entity::{AsEntity, Entity};
+use group::Group;
+use param::Param;
+
+pub mod constraint;
+pub mod entity;
+pub mod group;
+pub mod param;
 
 mod binding {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
@@ -30,23 +38,23 @@ impl TryFrom<i32> for SolveResult {
     }
 }
 
-pub struct System {
-    groups: Vec<binding::Slvs_hGroup>,
+pub struct System<'a> {
+    groups: Vec<Group>,
     next_group_h: u32,
-    params: Vec<binding::Slvs_Param>,
+    params: Vec<Param>,
     next_param_h: u32,
-    entities: Vec<binding::Slvs_Entity>,
+    entities: Vec<Entity>,
     next_entity_h: u32,
-    constraints: Vec<binding::Slvs_Constraint>,
+    constraints: Vec<Constraint>,
     next_constraint_h: u32,
     dragged: [binding::Slvs_hParam; 4],
     calculateFaileds: bool,
-    failed: Vec<Box<binding::Slvs_Constraint>>,
+    failed: Vec<&'a Constraint>,
     dof: i32,
     result: SolveResult,
 }
 
-impl System {
+impl System<'_> {
     pub fn new() -> Self {
         Self {
             groups: Vec::new(),
@@ -63,6 +71,188 @@ impl System {
             dof: 0,
             result: SolveResult::None,
         }
+    }
+
+    pub fn solve(&mut self, group: &Group) {
+        let mut failed_handles: Vec<binding::Slvs_hConstraint> = vec![0; self.constraints.len()];
+
+        let mut slvs_system = binding::Slvs_System {
+            param: self.params.as_mut_ptr(),
+            params: self.params.len() as _,
+            entity: self.entities.as_mut_ptr(),
+            entities: self.entities.len() as _,
+            constraint: self.constraints.as_mut_ptr(),
+            constraints: self.constraints.len() as _,
+            dragged: self.dragged,
+            calculateFaileds: self.calculateFaileds as _,
+            failed: failed_handles.as_mut_ptr(),
+            faileds: failed_handles.len() as _,
+            dof: self.dof,
+            result: self.result as _,
+        };
+
+        unsafe {
+            binding::Slvs_Solve(&mut slvs_system, (*group).into());
+
+            failed_handles = Vec::from_raw_parts(
+                slvs_system.failed,
+                slvs_system.faileds.try_into().unwrap(),
+                slvs_system.faileds.try_into().unwrap(),
+            )
+        };
+
+        self.failed = failed_handles
+            .into_iter()
+            .map(|h: binding::Slvs_hConstraint| self.get_constraint(h).unwrap())
+            .collect();
+        self.dof = slvs_system.dof;
+        self.result = slvs_system.result.try_into().unwrap();
+    }
+}
+
+// Methods for interfacing with group
+impl System<'_> {
+    pub fn add_group(&mut self) -> &Group {
+        let new_group = Group(self.next_group_h);
+        self.next_group_h += 1;
+
+        let groups = &mut self.groups;
+
+        groups.push(new_group);
+        groups.last().unwrap()
+    }
+}
+
+// Methods for interfacing with params
+impl System<'_> {
+    fn add_param(&mut self, group: &Group, val: f64) -> binding::Slvs_hParam {
+        let new_param = Param {
+            h: self.next_param_h,
+            group: (*group).into(),
+            val,
+        };
+        self.next_param_h += 1;
+
+        self.params.push(new_param);
+        self.params.last().unwrap().h
+    }
+
+    fn get_param(&self, h: binding::Slvs_hParam) -> Option<&Param> {
+        self.params.iter().find(|&param| param.h == h)
+    }
+}
+
+// Methods for interfacing with entities
+impl System<'_> {
+    pub fn add_entity(
+        &mut self,
+        group: &Group,
+        entity: impl AsEntity,
+    ) -> Result<&Entity, &'static str> {
+        let params: [u32; 4] = entity
+            .param_vals()
+            .iter()
+            .map(|&param| {
+                if let Some(val) = param {
+                    self.add_param(group, val)
+                } else {
+                    0
+                }
+            })
+            .collect::<Vec<u32>>()
+            .try_into()
+            .unwrap();
+
+        let new_entity = Entity {
+            h: self.next_entity_h,
+            group: (*group).into(),
+            type_: entity.type_() as _,
+            wrkpl: entity.wrkpl().unwrap_or(0), // TODO: requires check that entity with handle exists
+            point: entity.point().map(|p| p.unwrap_or(0)), // TODO: ditto
+            normal: entity.normal().unwrap_or(0), // TODO: ditto
+            distance: entity.distance().unwrap_or(0), // TODO: ditto
+            param: params,
+        };
+        self.next_entity_h += 1;
+
+        self.entities.push(new_entity);
+        Ok(self.entities.last().unwrap())
+    }
+
+    fn get_entity(&self, h: binding::Slvs_hEntity) -> Option<&Entity> {
+        self.entities.iter().find(|&entity| entity.h == h)
+    }
+}
+
+// Methods for interfacing with constraints
+impl System<'_> {
+    fn get_constraint(&self, h: binding::Slvs_hConstraint) -> Option<&Constraint> {
+        self.constraints
+            .iter()
+            .find(|&constraint| constraint.h == h)
+    }
+}
+
+impl Default for System<'_> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use crate::{element::entity::PointIn3d, System};
+
+    use crate::{entity::PointIn3d, System};
+
+    #[test]
+    fn solve_3d_demo() {
+        let mut sys = System::new();
+        let g = sys.add_group();
+        let p1 = sys.add_entity(
+            g,
+            PointIn3d {
+                x: 10.0,
+                y: 10.0,
+                z: 10.0,
+            },
+        );
+        let p2 = sys.add_entity(
+            g,
+            PointIn3d {
+                x: 20.0,
+                y: 20.0,
+                z: 20.0,
+            },
+        );
+
+        // let p1 = sys
+        //     .add_point_3d(g, 10.0, 10.0, 10.0)
+        //     .expect("Should be Handle::Entity");
+        // let p2 = sys
+        //     .add_point_3d(g, 20.0, 20.0, 20.0)
+        //     .expect("Should be Handle::Entity");
+        // sys.add_line_3d(g, p1, p2)
+        //     .expect("Should be Handle::Entity");
+        // sys.constrain_distance(g, None, 30.0, p1, p2)
+        //     .expect("Should be Handle::Constraint");
+
+        // sys.set_dragged(p2);
+        // sys.solve(g);
+        // sys.clear_dragged();
+
+        // assert_eq!(FailReason::None, sys.solve_result.reason);
+
+        // if let FailReason::None = sys.solve_result.reason {
+        //     println!(
+        //         "p1: ({:.3}, {:.3}, {:.3})",
+        //         sys.params[0].val, sys.params[1].val, sys.params[2].val
+        //     );
+        //     println!(
+        //         "p2: ({:.3}, {:.3}, {:.3})",
+        //         sys.params[3].val, sys.params[4].val, sys.params[5].val
+        //     );
+        // }
     }
 }
 
@@ -130,7 +320,6 @@ impl System {
 //         self.dragged = [0; 4];
 //     }
 
-//     // I want this to return a result.
 //     pub fn solve(&mut self, group: Handle) {
 //         let mut slvs_system = binding::Slvs_System {
 //             param: self.params.as_mut_ptr(),
@@ -268,44 +457,5 @@ impl System {
 // impl Default for System {
 //     fn default() -> Self {
 //         Self::new()
-//     }
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use crate::{FailReason, System};
-
-//     #[test]
-//     fn solve_3d_demo() {
-//         let mut sys = System::new();
-//         let g = sys.add_group();
-
-//         let p1 = sys
-//             .add_point_3d(g, 10.0, 10.0, 10.0)
-//             .expect("Should be Handle::Entity");
-//         let p2 = sys
-//             .add_point_3d(g, 20.0, 20.0, 20.0)
-//             .expect("Should be Handle::Entity");
-//         sys.add_line_3d(g, p1, p2)
-//             .expect("Should be Handle::Entity");
-//         sys.constrain_distance(g, None, 30.0, p1, p2)
-//             .expect("Should be Handle::Constraint");
-
-//         sys.set_dragged(p2);
-//         sys.solve(g);
-//         sys.clear_dragged();
-
-//         assert_eq!(FailReason::None, sys.solve_result.reason);
-
-//         if let FailReason::None = sys.solve_result.reason {
-//             println!(
-//                 "p1: ({:.3}, {:.3}, {:.3})",
-//                 sys.params[0].val, sys.params[1].val, sys.params[2].val
-//             );
-//             println!(
-//                 "p2: ({:.3}, {:.3}, {:.3})",
-//                 sys.params[3].val, sys.params[4].val, sys.params[5].val
-//             );
-//         }
 //     }
 // }
