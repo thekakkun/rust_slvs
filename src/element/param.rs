a52cdf8ee2882dd13f04f7d61a342bc9a9779076
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-04-10 13:13:57 -0400

    Re-create param struct

--- src/element/param.rs
+++ src/element/param.rs
@@ -1,17 +1,9 @@
-use std::sync::atomic::{AtomicU32, Ordering};
-
 use crate::binding;
-
-static NEXT_PARAM_H: AtomicU32 = AtomicU32::new(1);
 
 pub type Param = binding::Slvs_Param;
 
-impl Param {
-    pub(crate) fn new(group: binding::Slvs_hGroup, val: f64) -> Self {
-        Self {
-            h: NEXT_PARAM_H.fetch_add(1, Ordering::SeqCst),
-            group,
-            val,
-        }
+impl From<Param> for binding::Slvs_hParam {
+    fn from(value: Param) -> Self {
+        value.h
     }
 }
