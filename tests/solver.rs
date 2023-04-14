9586f1bae0c5902fca7370fb4d42bcf2905561b7
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-04-14 15:34:17 -0400

    Move solver test into its own integration test

---
+++ tests/solver.rs
@@ -0,0 +1,78 @@
+use slvs::{
+    constraint::PtPtDistance,
+    entity::{LineSegment, PointIn3d},
+    System,
+};
+
+const SOLVE_TOLERANCE: f64 = 1e-8;
+
+#[test]
+fn solve_3d_demo() {
+    let mut sys = System::new();
+    let g = sys.add_group();
+    let p1 = sys
+        .add_entity(
+            g,
+            PointIn3d {
+                x: 10.0,
+                y: 10.0,
+                z: 10.0,
+            },
+        )
+        .expect("p1 created");
+
+    let p2 = sys
+        .add_entity(
+            g,
+            PointIn3d {
+                x: 20.0,
+                y: 20.0,
+                z: 20.0,
+            },
+        )
+        .expect("p2 created");
+
+    sys.add_entity(
+        g,
+        LineSegment {
+            point_a: p1,
+            point_b: p2,
+        },
+    )
+    .expect("line segment created");
+
+    let target_dist = 30.0;
+    sys.add_constraint(
+        g,
+        PtPtDistance::_3d {
+            val: target_dist,
+            point_a: p1,
+            point_b: p2,
+        },
+    )
+    .expect("distance constraint added");
+
+    sys.set_dragged(p2);
+    let solve_result = sys.solve(g);
+    sys.clear_dragged();
+
+    if solve_result.is_ok() {
+        let new_p1: PointIn3d = sys
+            .get_entity_data(p1)
+            .expect("p1 should exist")
+            .try_into()
+            .expect("p1 is PointIn3d");
+        let new_p2: PointIn3d = sys
+            .get_entity_data(p2)
+            .expect("p2 should exist")
+            .try_into()
+            .expect("p2 is PointIn3d");
+
+        let dist = ((new_p1.x - new_p2.x).powi(2)
+            + (new_p1.y - new_p2.y).powi(2)
+            + (new_p1.z - new_p2.z).powi(2))
+        .sqrt();
+
+        assert!((target_dist - dist).abs() < SOLVE_TOLERANCE);
+    }
+}
