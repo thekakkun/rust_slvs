c3ef0321d3849cd7ad8933441ae978cc124a1f40
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-04-22 15:55:10 -0400

    Use updated interface

--- tests/solver.rs
+++ tests/solver.rs
@@ -1,7 +1,7 @@
 use slvs::{
-    constraint::PtPtDistance,
-    entity::{LineSegment, PointIn3d},
-    FailReason, System,
+    constraint::{Constraint, PtPtDistance},
+    entity::{Coords, LineSegment, Point},
+    FailReason, In3d, System,
 };
 
 const SOLVE_TOLERANCE: f64 = 1e-8;
@@ -10,26 +10,20 @@ const SOLVE_TOLERANCE: f64 = 1e-8;
 fn solve_3d_demo() {
     let mut sys = System::new();
     let g = sys.add_group();
+
     let p1 = sys
-        .add_entity(&g, PointIn3d::new(10.0, 10.0, 10.0))
+        .sketch_in_3d(&g, Point::<In3d>::new(10.0, 10.0, 10.0))
         .expect("p1 created");
     let p2 = sys
-        .add_entity(&g, PointIn3d::new(20.0, 20.0, 20.0))
+        .sketch_in_3d(&g, Point::<In3d>::new(20.0, 20.0, 20.0))
         .expect("p2 created");
 
-    sys.add_entity(&g, LineSegment::new_in_3d(p1, p2))
+    sys.sketch_in_3d(&g, LineSegment::<In3d>::new(p1, p2))
         .expect("line segment created");
 
     let target_dist = 30.0;
-    sys.add_constraint(
-        &g,
-        PtPtDistance::_3d {
-            val: target_dist,
-            point_a: p1,
-            point_b: p2,
-        },
-    )
-    .expect("distance constraint added");
+    sys.constrain_in_3d(&g, PtPtDistance::<In3d>::new(p1, p2, target_dist))
+        .expect("distance constraint added");
 
     sys.set_dragged(&p2);
     let solve_result = sys.solve(&g);
@@ -39,12 +33,22 @@ fn solve_3d_demo() {
         let new_p1 = sys.entity_data(&p1).expect("p1 should exist");
         let new_p2 = sys.entity_data(&p2).expect("p2 should exist");
 
-        let dist = ((new_p1.x - new_p2.x).powi(2)
-            + (new_p1.y - new_p2.y).powi(2)
-            + (new_p1.z - new_p2.z).powi(2))
-        .sqrt();
-
-        assert!((target_dist - dist).abs() < SOLVE_TOLERANCE);
+        if let (
+            Coords::In3d {
+                x: x1,
+                y: y1,
+                z: z1,
+            },
+            Coords::In3d {
+                x: x2,
+                y: y2,
+                z: z2,
+            },
+        ) = (new_p1.coords, new_p2.coords)
+        {
+            let dist = ((x1 - x2).powi(2) + (y1 - y2).powi(2) + (z1 - z2).powi(2)).sqrt();
+            assert!((target_dist - dist).abs() < SOLVE_TOLERANCE);
+        }
     }
 }
 
@@ -52,35 +56,21 @@ fn solve_3d_demo() {
 fn inconsistent_constraints() {
     let mut sys = System::new();
     let g = sys.add_group();
-    let p1 = sys
-        .add_entity(&g, PointIn3d::new(10.0, 10.0, 10.0))
-        .expect("p1 created");
 
+    let p1 = sys
+        .sketch_in_3d(&g, Point::<In3d>::new(10.0, 10.0, 10.0))
+        .expect("p1 created");
     let p2 = sys
-        .add_entity(&g, PointIn3d::new(20.0, 20.0, 20.0))
+        .sketch_in_3d(&g, Point::<In3d>::new(20.0, 20.0, 20.0))
         .expect("p2 created");
 
     // distance between p1 and p2 is 10
     let c1 = sys
-        .add_constraint(
-            &g,
-            PtPtDistance::_3d {
-                val: 10.0,
-                point_a: p1,
-                point_b: p2,
-            },
-        )
+        .constrain_in_3d(&g, PtPtDistance::<In3d>::new(p1, p2, 10.0))
         .expect("distance constraint added");
     // distance between p1 and p2 is 20
     let c2 = sys
-        .add_constraint(
-            &g,
-            PtPtDistance::_3d {
-                val: 20.0,
-                point_a: p1,
-                point_b: p2,
-            },
-        )
+        .constrain_in_3d(&g, PtPtDistance::<In3d>::new(p1, p2, 20.0))
         .expect("distance constraint added");
 
     let solve_result = sys.solve(&g);
